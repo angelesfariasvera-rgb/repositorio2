c803dd185badffd847e81fa160ca15568a794d29
Angeles <angelesfariasvera@gmail.com>
2026-05-08 11:19:11 -0400

    Actualización con ejercicios hechos. Cosas pendientes: crear el README

--- ejercicios1.js
+++ ejercicios1.js
@@ -1,1 +1,55 @@
 console.log("Hola mundo. Esta es la segunda prueba");
+console.log("Ahora vamos a hacer algunos ejercicios de programación básica");
+
+console.log("Ejercicio 1: Crea dos variables: una con tu nombre y otra con tu apellido. Imprime ambos juntos en un solo console.log");
+let nombre = "Angeles";
+let apellido = "Farías";
+console.log("Nombre: " + nombre + " Apellido: " + apellido);
+
+console.log("Ejercicio 2: Crea una variable con un número. Súmale 5, luego multiplícalo por 3 e imprime el resultado.");
+let a = 10; let b = 5; let c = 3;
+let resultado = (a + b) * c;
+console.log("El resultado es: " + resultado);
+
+console.log("Ejercicio 3: Crea una variable con tu comida favorita e imprímela en consola.");
+let comidaFavorita = "Carne Mongoliana";
+console.log("Mi comida favorita es: " + comidaFavorita);
+
+console.log("Ejercicio 4: Crea dos variables a = 10 y b = 3. Imprime el resultado de las siguientes operaciones: La suma, La resta, La multiplicación, el módulo.");
+a = 10; b = 3;
+console.log("Suma: " + (a + b));
+console.log("Resta: " + (a - b));
+console.log("Multiplicación: " + (a * b));
+console.log("Módulo: " + (a % b));
+
+console.log("Ejercicio 5: Crea una variable `edad = 15`Imprime si:es mayor a 10 es menor a 18");
+let edad = 15;
+console.log(edad > 10 && edad < 18);
+
+console.log("Ejercicio 6: Crea una variable `numero = 8` Imprime si: es mayor que 5 Y menor que 10");
+let numero = 8;
+console.log(numero > 5 && numero < 10);
+
+console.log("Ejercicio 7: Crea una variable `nota = 6`Imprime si es igual a 7");
+let nota = 6;
+console.log(nota === 7);
+
+console.log("Ejercicio 8: Crea una variable `temperatura = 25` Imprime si: es mayor o igual a 30");
+let temperatura = 25;
+console.log(temperatura >= 30);
+
+console.log("Ejercicio 9: Crea dos números. Imprime si son iguales y si uno es mayor que el otro");
+let num1 = 5; let num2 = 10;
+console.log("Valor 1 = 5 Valor 2 = 10");
+console.log("¿Son iguales? " + (num1 === num2));
+console.log("¿num1 es mayor que num2? " + (num1 > num2));
+console.log("¿num2 es mayor que num1? " + (num2 > num1));
+
+console.log("Ejercicio 10: let edad=17; let tienePermiso=true e imprime si: la edad es mayor o igual a 18 O tiene permiso para conducir");
+edad = 17; let tienePermiso = true;
+console.log(edad >= 18 || tienePermiso);
+
+console.log("Ejercicio 11: crear una cuenta con nombre de usuario "admin" Y password es "1234"");
+let username = "admin"; let password = "1234";
+console.log("Nombre de usuario: " + username + " Contraseña: " + password);
+
